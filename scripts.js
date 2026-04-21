e160148bdb0d3dda7ffc129aab81f7c1befc3a0c
Cristea Alexandru <speedgun6@gmail.com>
2026-04-21 18:04:20 +0300

    Add third API

--- scripts.js
+++ scripts.js
@@ -2,3 +2,4 @@ console.log("Hello, World!");
 
 const API_1 : string = "https://api.example.com/data1";
 const API_2 : string = "https://api.example.com/data2";
+const API_3 : string = "https://api.example.com/data3";
